6468e5bb63998192498d7e595bbb9169b1986cc4
Viki Designs <vikdemo2023@gmail.com>
2024-01-10 16:56:30 +0300

    second commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,13 @@
-// Add some js 
+const sliderEl = document.getElementById("slider");
+const storageEl = document.querySelector(".storage");
+let availableStorageEl = document.querySelector(".available-storage p span");
+
+sliderEl.addEventListener("input", () => {
+  storageEl.textContent = sliderEl.value + " GB";
+  availableStorageEl.textContent = 1000 - sliderEl.value;
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const result = storageEl.textContent.replace("GB", "");
+  sliderEl.value = +result;
+});
